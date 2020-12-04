ce646e8519ea957eb54f8a7cfab64076631706ba
jpgfigueiredo <johnpeterfigueiredo@gmail.com>
2020-12-04 14:25:43 -0300

    Projeto criado

---
+++ src/main/java/com/cruso/spring/boot/jp/CursoSpingBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.cruso.spring.boot.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpingBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpingBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cruso/spring/boot/jp/CursoSpingBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruso.spring.boot.jp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpingBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
