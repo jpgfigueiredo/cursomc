0054d74e9b5bede7db92dfbbcc1860266e8b28e5
jpgfigueiredo <johnpeterfigueiredo@gmail.com>
2020-12-06 14:19:56 -0300

    Criando operação de instanciação de Categorias

--- src/main/java/com/cruso/spring/boot/CursoSpingBootUdemyApplication.java
+++ src/main/java/com/cruso/spring/boot/CursoSpingBootUdemyApplication.java
@@ -1,13 +1,31 @@
 package com.cruso.spring.boot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpingBootUdemyApplication {
+import com.cruso.spring.boot.domain.Categoria;
+import com.cruso.spring.boot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpingBootUdemyApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpingBootUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
